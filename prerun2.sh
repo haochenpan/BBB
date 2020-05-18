04965c1df63ead61e94b28bfe6c42d43281c16f5
Haochen Pan <32943456+haochenpan@users.noreply.github.com>
2020-05-18 10:40:27 -0400

    change keys

--- prerun2.sh
+++ prerun2.sh
@@ -21,11 +21,14 @@ while [ $cur_miner -le $num_of_miners ]; do
 
   # geth init
   geth init --datadir ${!dirName} ~/BBB/genesis.json
+
+  # copy keys
+  cp ~/BBB/keys/UTC--2020-03-07T23-25-18.301273092Z--67e37abe6fb7bb2b0d61b9c6f53c71623ae65551 ~/BBB/hosts/ethData$cur_miner/keystore
+  cp ~/BBB/keys/UTC--2020-03-07T23-27-12.170957487Z--2dec65f7f6fecef9088afed7ab41ad0f1173ddb4 ~/BBB/hosts/ethData$cur_miner/keystore
+  cp ~/BBB/keys/UTC--2020-03-07T23-27-53.050585459Z--0213af577d12cf11a5baf5a869e0b1305684ca0a ~/BBB/hosts/ethData$cur_miner/keystore
+  cp ~/BBB/keys/UTC--2020-03-16T21-29-01.688713168Z--7d8466475a66c4363da52494af4a3c20298f5f73 ~/BBB/hosts/ethData$cur_miner/keystore
   ((cur_miner++))
 done
 
-## copy 4 keys, no need to copy a key for each host
-#cp ~/BBB/keys/UTC--2020-03-07T23-25-18.301273092Z--67e37abe6fb7bb2b0d61b9c6f53c71623ae65551 $DDR1/keystore
-#cp ~/BBB/keys/UTC--2020-03-07T23-27-12.170957487Z--2dec65f7f6fecef9088afed7ab41ad0f1173ddb4 $DDR2/keystore
-#cp ~/BBB/keys/UTC--2020-03-07T23-27-53.050585459Z--0213af577d12cf11a5baf5a869e0b1305684ca0a $DDR3/keystore
-#cp ~/BBB/keys/UTC--2020-03-16T21-29-01.688713168Z--7d8466475a66c4363da52494af4a3c20298f5f73 $DDR4/keystore
+
+
